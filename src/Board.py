cdfec05c799baa49442237e09dcce41f010c558e
conkindred <102344185+conkindred@users.noreply.github.com>
2022-04-19 18:55:11 -0600

    Update and rename Board to Board.py

---
+++ src/Board.py
@@ -0,0 +1,17 @@
+
+class Board():
+  def __init__(self): 
+    self.grid = [[0,0,0],[0,0,0],[0,0,0]] 
+  def print_self(self): 
+      print (self.grid[0])
+      print (self.grid[1])
+      print (self.grid[2])
+  def is_win(self):
+    # not yet built
+    print("not yet built")
+  def take_turnX(self):
+    # not yet built
+    print("not yet built")
+  def take_turnO(self):
+    # not yet built
+    print("not yet built")
