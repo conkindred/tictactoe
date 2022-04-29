e8840573073416d1c030dda7d775a5943b2898ca
conkindred <102344185+conkindred@users.noreply.github.com>
2022-04-28 19:17:47 -0600

    Update Board.py

--- src/Board.py
+++ src/Board.py
@@ -1,4 +1,3 @@
-
 class Board():
   def __init__(self): 
     self.grid = [[0,0,0],[0,0,0],[0,0,0]] 
@@ -15,3 +14,6 @@ class Board():
   def take_turnO(self):
     # not yet built
     print("not yet built")
+  def get_board_img(self):
+    board_as_string = str(self.grid[0][0]) + " | " + str(self.grid[0][1]) + " | "+ str(self.grid[0][2])+ "\n---------\n"+ str(self.grid[1][0]) + " | " + str(self.grid[1][1]) + " | "+ str(self.grid[1][2])+ "\n---------\n"+ str(self.grid[2][0]) + " | " + str(self.grid[2][1]) + " | "+ str(self.grid[2][2])
+    return board_as_string 
